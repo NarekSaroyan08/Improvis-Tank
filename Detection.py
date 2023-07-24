1f4ddf5de46ac81db0a06cfe78a2688e0e506d7d
NarekSaroyan08 <127611134+NarekSaroyan08@users.noreply.github.com>
2023-07-24 18:04:23 +0400

    Add files via upload

---
+++ Detection.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+ 
+model = YOLO("yolov8m.pt")  # load a pretrained YOLOv8n model
+ 
+model.train(data="args.yaml")  # train the model
+model.val()  # evaluate model performance on the validation set
+model.predict(source="https://ultralytics.com/images/bus.jpg")  # predict on an image
+model.export(format="onnx")  # export the model to ONNX format
